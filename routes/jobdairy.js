085e2c5d4af0ee31ba6f9f79253da853e68fb9af
parkjuhuck <gusehdcks3@gmail.com>
2018-05-01 15:05:20 +0900

    test

--- routes/jobdairy.js
+++ routes/jobdairy.js
@@ -3,4 +3,9 @@ module.exports = function(app) {
     app.get('/test', function(req, res, next) {
         res.status(200).json('test');
     });
+    app.get('/test2', function(req, res, next) {
+        var output = {};
+        output.msg = 'success';
+        res.status(200).json(output);
+    });
 }
