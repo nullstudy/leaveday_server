11cb6af4a37d82899566bc244a32353bc01a34fa
parkjuhuck <gusehdcks3@gmail.com>
2018-05-20 04:58:10 +0900

    #11 구글로그인완료

--- model/Jobdairydb.js
+++ model/Jobdairydb.js
@@ -7,7 +7,11 @@ const diarySchema = new Schema({
     leaveCount: Number, //예측일
     content: String,  // 글내용
     status: Number // 상태  0 안끝남 1 끝남 2 보류 
+<<<<<<< HEAD
     // private: Number, 
+=======
+    // private: Number 
+>>>>>>> ff1407f... #11코드리뷰수정
     // favorite: Number 
 });
 const diaryModel = mongoose.model('Jobdiary', diarySchema);
