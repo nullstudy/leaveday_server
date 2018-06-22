69fee60642e6fff9dac8e81b6134a462491f9c3d
parkjuhuck <gusehdcks3@gmail.com>
2018-06-22 19:47:11 +0900

    #22 diary-디테일,수정 기능  -diary 보기, diary 수정기능 완료

--- controllers/jobdiary.js
+++ controllers/jobdiary.js
@@ -8,15 +8,15 @@ exports.createDiary =async function(req,res,next){ //leaveDay 정보입력
         let tokenCheck = req.body.tokenData;
         let userInfo = req.body.userInfo;
         if (userInfo) {
-            for(var i =0; i<40; i++){
+            // for(var i =0; i<40; i++){
                 let diary = new DiaryModel();
                 diary.author = String(userInfo[0]._id);
-                diary.title = String(req.body.title+'테스트'+i);
+                diary.title = String(req.body.title+'테스트');
                 diary.content = String(req.body.content);
                 diary.state = Number(req.body.state);
                 diary.leaveCount = Number(req.body.leaveCount);
                 await dbQuery.save(diary);
-            }
+            // }
             output.msg = 'success';
             res.status(200).send(output);
         } else {
@@ -33,7 +33,7 @@ exports.createDiary =async function(req,res,next){ //leaveDay 정보입력
 }
 
 
-exports.getDiary =async function(req,res,next){ //leaveDay 정보입력
+exports.getDiary =async function(req,res,next){ //leaveDay list
     const output = {};
     try {
         let tokenCheck = req.body.tokenData;
@@ -57,13 +57,21 @@ exports.getDiary =async function(req,res,next){ //leaveDay 정보입력
                     state :  1 
                     }
                 },
-                { $sort: sort},
-                {"$skip":pageCount},
-                {"$limit":count}
+                { $sort: sort },
+                {"$skip":pageCount },
+                {"$limit":count }
             ];
             let getJobDiary = await dbQuery.aggregate(DiaryModel, findData); 
-            console.log(getJobDiary)
+            
+            for(var item in getJobDiary){
+                if(getJobDiary[item].state == 1) getJobDiary[item].state = { state: '행복', number : 1 }
+                if(getJobDiary[item].state == 2) getJobDiary[item].state = { state: '평온', number : 2 }
+                if(getJobDiary[item].state == 3) getJobDiary[item].state = { state: '별로', number : 3 }
+                if(getJobDiary[item].state == 4) getJobDiary[item].state = { state: '화남', number : 4 }
+                if(getJobDiary[item].state == 5) getJobDiary[item].state = { state: '빡침', number : 5 }
+            }
             output.data = getJobDiary;
+        
             output.msg = 'success';
             res.status(200).send(output);
         } else {
@@ -79,3 +87,81 @@ exports.getDiary =async function(req,res,next){ //leaveDay 정보입력
     }
 }
 
+exports.getDetailDiary = async function(req,res,next) {
+    const output = {};
+    try {
+        if (req.params._id) {
+
+            let findData = { "_id": ObjectId(req.params._id) };
+            let updateData = { $inc: { views: +1 } }
+            let viewsUpdate = await dbQuery.updateDate(DiaryModel, findData,updateData); 
+
+            findData =  [
+                { $match : {  _id : ObjectId(req.params._id)}},
+                { $project : {
+                    _id : 1,
+                    title : 1,
+                    content : 1,
+                    views : 1,
+                    author: 1,
+                    date : { $dateToString: { format: "%Y.%m.%d", date: "$date" } },
+                    leaveCount : 1,
+                    state :  1 
+                    }
+                }
+            ];
+            let getJobDiary = await dbQuery.aggregate(DiaryModel, findData); 
+            
+            for(var item in getJobDiary){
+                if(getJobDiary[item].state == 1) getJobDiary[item].state = { state: '행복', number : 1 }
+                if(getJobDiary[item].state == 2) getJobDiary[item].state = { state: '평온', number : 2 }
+                if(getJobDiary[item].state == 3) getJobDiary[item].state = { state: '별로', number : 3 }
+                if(getJobDiary[item].state == 4) getJobDiary[item].state = { state: '화남', number : 4 }
+                if(getJobDiary[item].state == 5) getJobDiary[item].state = { state: '빡침', number : 5 }
+            }
+            output.data = getJobDiary;
+            output.msg = 'success';
+            res.status(200).send(output);
+        } else {
+            output.msg = 'data not exist';
+            res.status(200).send(output);
+        }
+    } catch (e) {
+        console.log(e);
+        output.msg = 'fail';
+        res.setHeader('Authorization', tokenCheck.token);
+        res.status(404).send(output);
+    }
+}
+
+
+exports.editDiary = async function(req,res,next) {
+    const output = {};
+    try {
+        console.log(req.params._id)
+        if (req.params._id) {
+
+            let findData = { "_id": ObjectId(req.params._id) };
+            let diaryInfo = {
+                "title": String(req.body.title),
+                "content": String(req.body.content),
+                "state": Number(req.body.state),
+                "leaveCount": Number(req.body.leaveCount)
+            }
+
+            let updateData = { "$set": diaryInfo };
+            await dbQuery.updateDate(DiaryModel, findData, updateData);
+            output.data = dbQuery;
+            output.msg = 'success';
+            res.status(200).send(output);
+        } else {
+            output.msg = 'data not exist';
+            res.status(200).send(output);
+        }
+    } catch (e) {
+        console.log(e);
+        output.msg = 'fail';
+        res.setHeader('Authorization', tokenCheck.token);
+        res.status(404).send(output);
+    }
+}
--- routes/jobdiary.js
+++ routes/jobdiary.js
@@ -5,7 +5,7 @@ const diaryController = require('../controllers/jobdiary');
 
 module.exports = function(app) {
     app.get('/jobDiary',jsonWebToken.authMiddleware,userUtil.userVerify,diaryController.getDiary);
+    app.put('/jobDiary/edit/:_id',diaryController.editDiary);
+    app.get('/jobDiary/detail/:_id',diaryController.getDetailDiary);
     app.post('/jobDiary/create',jsonWebToken.authMiddleware,userUtil.userVerify,diaryController.createDiary);
-    
-    
 }
