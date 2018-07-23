d9fc15a4ec6e35f0bb86130c7e981d9a29668eb1
parkjuhuck <gusehdcks3@gmail.com>
2018-07-23 16:10:10 +0900

    # aws 로그인 테스트

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Authorization,Content-Type, Origin, X-Requested-With,  Accept");
     next();
 });
+
 var session = require('express-session');
 var passport = require('passport');
 
